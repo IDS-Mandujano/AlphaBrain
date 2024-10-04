94313e409a66e7cb6a1fe76cdd9b3686529f333f
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-10-04 10:21:28 -0600

    RESOLVE PROBLEMS

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoComponent } from './video/video.component';
 import { FormsModule } from '@angular/forms';
-import { PostsComponent } from './posts/posts.component';
 import { FormComponent } from './form/form.component';
-
-
 
 @NgModule({
   declarations: [
     VideoComponent,
-    PostsComponent,
     FormComponent
   ],
   imports: [
